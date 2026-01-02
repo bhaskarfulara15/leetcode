c12a24f2b76f841f516b4ee2565a0a64a281bc00
Bhaskar Fulara <bhaskarfulara02@gmail.com>
2026-01-02 11:35:29 +0530

    Create try2.py

---
+++ try2.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def rotateRight(self, head, k):
+        if not head or not head.next or k==0:
+            return head
+
+        length=1
+        tail=head
+        while tail.next:
+            length=length+1
+            tail=tail.next
+
+        k%=length
+        if k==0:
+            return head
+
+        tail.next=head
+        step=length-k
+        temp=head
+
+        for i in range (step-1):
+            temp=temp.next
+
+        new_head=temp.next
+        temp.next=None
+
+        return new_head  
