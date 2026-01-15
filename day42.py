136d9e8206fd28cebe1cca02ebe1dd1c11ebd6c4
Bhaskar Fulara <bhaskarfulara02@gmail.com>
2026-01-15 19:10:42 +0530

    Create day42.py

---
+++ day42.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def firstBadVersion(self, n):
+        low=1
+        high=n
+        while low<high:
+            mid=(high+low)//2
+            if isBadVersion(mid):
+                high=mid
+            else:
+                low=mid+1
+
+        return low
