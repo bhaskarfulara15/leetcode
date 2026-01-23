3ac06f63c8515f5e475bc99bda50ce0f8641a314
Bhaskar Fulara <bhaskarfulara02@gmail.com>
2026-01-23 20:09:17 +0530

    Update day49.py

--- day49.py
+++ day49.py
@@ -1,35 +1,25 @@
-class Solution:
+class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
-
-        m, n = len(nums1), len(nums2)
-        left, right = 0, m
-
-        while left <= right:
-            i = (left + right) // 2
-            j = (m + n + 1) // 2 - i
-
-            if i > 0 and j < n and nums1[i - 1] > nums2[j]:
-                right = i - 1
-            elif i < m and j > 0 and nums2[j - 1] > nums1[i]:
-                left = i + 1
+        midpoint = (len(nums1)+len(nums2))//2 + 1
+        i1= 0
+        i2 = 0
+        median = 0
+        lag_med = 0
+        for i in range(midpoint):
+            lag_med = median
+            if i1 < len(nums1) and i2 < len(nums2):
+                if nums1[i1] < nums2[i2]:
+                    median = nums1[i1]
+                    i1 += 1
+                else:
+                    median = nums2[i2]
+                    i2 += 1
+            elif(i2 >= len(nums2)):
+                median = nums1[i1]
+                i1 += 1
             else:
-                if i == 0:
-                    left_max = nums2[j - 1]
-                elif j == 0:
-                    left_max = nums1[i - 1]
-                else:
-                    left_max = max(nums1[i - 1], nums2[j - 1])
-
-                if (m + n) % 2 == 1:
-                    return left_max
-
-                if i == m:
-                    right_min = nums2[j]
-                elif j == n:
-                    right_min = nums1[i]
-                else:
-                    right_min = min(nums1[i], nums2[j])
-
-                return (left_max + right_min) / 2
+                median = nums2[i2]
+                i2 += 1
+        if not ((len(nums1)+len(nums2))%2):
+            return (float(median) + float(lag_med)) / 2
+        return median
