046cdb1afa76130ce964a52a0494527781138673
Bhaskar Fulara <bhaskarfulara02@gmail.com>
2026-02-20 20:50:30 +0530

    Implement findRestaurant method to find common restaurants

--- day75.java
+++ day75.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i=0;i<list1.length;i++){
+            map.put(list1[i],i);
+        }
+        int minSum=Integer.MAX_VALUE;
+        List<String> result=new ArrayList<>();
+        for(int j=0;j<list2.length;j++){
+            if(map.containsKey(list2[j])){
+                int currentSum=j+map.get(list2[j]);
+                if(currentSum<minSum){
+                    minSum=currentSum;
+                    result.clear();
+                    result.add(list2[j]);
+                } else if(currentSum==minSum){
+                    result.add(list2[j]);
+                }
+            }
+        }
+        
+        return result.toArray(new String[0]);
+    }
+}
