27ff5962c5e57211a0110cdb3004b4a32d03fd56
Bhaskar Fulara <bhaskarfulara02@gmail.com>
2026-02-21 20:22:38 +0530

    Implement sortArrayByParityII to sort by parity

--- day76.java
+++ day76.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int n=nums.length;
+        int[] result=new int[n];
 
+        int even=0;
+        int odd=1;
+        for(int i=0;i<n;i++){   //for(num:nums)
+            int num=nums[i];
+            if(num%2==0){
+                result[even]=num;
+                even=even+2;
+            }
+            else{
+                result[odd]=num;
+                odd=odd+2;
+            }
+        }
+        return result; 
+    }
+}
