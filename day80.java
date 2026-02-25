6993b21bc90a17ef939eefa00cb8f19e9a63d06d
Bhaskar Fulara <bhaskarfulara02@gmail.com>
2026-02-25 18:50:10 +0530

    Add lastStoneWeight method to Solution class
    Implement lastStoneWeight method to determine the weight of the last remaining stone.

--- day80.java
+++ day80.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int n=stones.length;
+        while(n>1){
+            Arrays.sort(stones,0,n);
+            int y=stones[n-1];
+            int x=stones[n-2];
+            if(x==y){
+                n=n-2;
+            }
+            else{
+                stones[n-2]=y-x;
+                n=n-1;
+            }
+        }
+        if(n==1){
+            return stones[0];
+        }
+        else{
+            return 0;
+        }
+    }
+}
