61cab81436a5c3be438e422087de63616297a888
Bhaskar Fulara <bhaskarfulara02@gmail.com>
2026-05-20 09:52:38 +0530

    day160.java from git

---
+++ day160.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int max_=0;
+        int n=A.length;
+        int[] ans=new int[n];
+
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<=i;j++){
+                for(int k=0;k<=i;k++){
+                    if(A[j]==B[k]){
+                        count++;
+                        break;
+                    }
+                }
+            }
+            ans[i]=count;
+        }
+        return ans;
+    }
+}
