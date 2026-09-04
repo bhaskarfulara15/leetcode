722001202dcb3d183de8d279d18fb3457e0f090d
Bhaskar Fulara <bhaskarfulara02@gmail.com>
2026-09-04 20:14:31 +0530

    day263.java from git

---
+++ day263.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int[] suffixMin = new int[n];
+        suffixMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+        }
+        int maxLeft = nums[0];
+        for (int i = 0; i < n; i++) {
+            maxLeft = Math.max(maxLeft, nums[i]);
+            int instability = maxLeft - suffixMin[i];
+            if (instability <= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
