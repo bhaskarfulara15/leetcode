f26a773b6401f081b75985bd655c4089d1c755f5
Bhaskar Fulara <bhaskarfulara02@gmail.com>
2026-09-15 20:37:52 +0530

    day274.java from git

---
+++ day274.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] mapST = new int[256];
+        int[] mapTS = new int[256];
+        for (int i = 0; i < 256; i++) {
+            mapST[i] = -1;
+            mapTS[i] = -1;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (mapST[c1] != -1 && mapST[c1] != c2) {
+                return false;
+            }
+            if (mapTS[c2] != -1 && mapTS[c2] != c1) {
+                return false;
+            }
+            mapST[c1] = c2;
+            mapTS[c2] = c1;
+        }
+        return true;
+    }
+}
